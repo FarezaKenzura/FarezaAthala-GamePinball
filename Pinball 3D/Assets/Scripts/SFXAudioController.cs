c159af2063ef093332612833b40e4f7dbe57f1b3
Fareza Athala <92410692+FarezaKenzura@users.noreply.github.com>
2023-10-06 10:20:27 +0700

    UI & Scene management

--- Pinball 3D/Assets/Scripts/SFXAudioController.cs
+++ Pinball 3D/Assets/Scripts/SFXAudioController.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SFXAudioController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 1.0f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
